fb6da1c2aab7479b0270e3e167e671cbeade73f0
Leo <zmliu0077@gmail.com>
2017-08-06 10:07:39 +0800

    init project

---
+++ app.js
@@ -0,0 +1,42 @@
+const koa = require('koa');
+const app = new koa();
+app.use(async (ctx, next) => {
+  const start = new Date;
+  console.log(`Started at: ${start}`);
+  await next();
+  const ms = new Date - start;
+  console.log(`Elapsed: ${ms}ms`);
+});
+
+
+
+//
+//
+// // x-response-time
+// app.use(function *(next){
+//   // (1) 进入路由
+//   var start = new Date;
+//   yield next;
+//   // (5) 再次进入 x-response-time 中间件，记录2次通过此中间件「穿越」的时间
+//   var ms = new Date - start;
+//   this.set('X-Response-Time', ms + 'ms');
+//   // (6) 返回 this.body
+// });
+
+// logger
+app.use(async (ctx, next) =>{
+  // (2) 进入 logger 中间件
+  const start = new Date;
+  await next();
+  // (4) 再次进入 logger 中间件，记录2次通过此中间件「穿越」的时间
+  const ms = new Date - start;
+  console.log('%s %s - %s', ctx.method, ctx.url, ms);
+});
+
+
+
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello world';
+});
+app.listen(3000);
