cd01a10c3cfba56027dae21d2592968f3645dfe3
Leo <zmliu0077@gmail.com>
2017-08-06 15:35:48 +0800

    add ts linet rule

--- config/initializer/database.js
+++ config/initializer/database.js
@@ -1,32 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// import * as config from 'config'
-// import logger from '../../app/lib/Logger'
-var mongoose = require("mongoose");
-// const mongoConfigs = config.get('database.mongodb')
-// const DEBUG_FLAG = config.get('database.mongoDebug')
-// mongoose.set('Promise', require('bluebird'))
-// mongoose.Promise = require('bluebird')
+const mongoose = require("mongoose");
 mongoose.set('debug', true);
-var dbs = new Map();
-function createConnection(url, options) {
-    if (options === void 0) { options = {}; }
-    var db = mongoose.createConnection(url, options);
-    db.on('error', function (err) {
-        err.message = "[mongoose]" + err.message;
+let dbs = new Map();
+function createConnection(url, options = {}) {
+    const db = mongoose.createConnection(url, options);
+    db.on('error', err => {
+        err.message = `[mongoose]${err.message}`;
         // logger.error(err)
         console.log(err);
     });
-    db.on('disconnected', function () {
-        console.log("[mongoose] " + url + " disconnected");
+    db.on('disconnected', () => {
+        console.log(`[mongoose] ${url} disconnected`);
         // logger.error(`[mongoose] ${url} disconnected`)
     });
-    db.on('connected', function () {
-        console.log("[mongoose] " + url + " connected successfully");
+    db.on('connected', () => {
+        console.log(`[mongoose] ${url} connected successfully`);
         // logger.info(`[mongoose] ${url} connected successfully`)
     });
-    db.on('reconnected', function () {
-        console.log("[mongoose] " + url + " reconnected successfully");
+    db.on('reconnected', () => {
+        console.log(`[mongoose] ${url} reconnected successfully`);
         // logger.info(`[mongoose] ${url} reconnected successfully`)
     });
     return db;
@@ -35,3 +28,4 @@ function createConnection(url, options) {
 dbs.set('db', createConnection('mongodb://127.0.0.1:27017/beta_koa', {}));
 // }
 exports.default = dbs;
+//# sourceMappingURL=database.js.map
--- config/initializer/database.ts
+++ config/initializer/database.ts
@@ -1,12 +1,5 @@
-// import * as config from 'config'
-// import logger from '../../app/lib/Logger'
 import * as mongoose from 'mongoose'
 
-// const mongoConfigs = config.get('database.mongodb')
-// const DEBUG_FLAG = config.get('database.mongoDebug')
-
-// mongoose.set('Promise', require('bluebird'))
-// mongoose.Promise = require('bluebird')
 mongoose.set('debug', true)
 let dbs: Map<string, mongoose.Connection> = new Map()
 
@@ -15,21 +8,21 @@ function createConnection (url, options = {}) {
   db.on('error', err => {
     err.message = `[mongoose]${err.message}`
     // logger.error(err)
-    console.log(err);
+    console.log(err)
   })
 
   db.on('disconnected', () => {
-    console.log(`[mongoose] ${url} disconnected`);
+    console.log(`[mongoose] ${url} disconnected`)
     // logger.error(`[mongoose] ${url} disconnected`)
   })
 
   db.on('connected', () => {
-    console.log(`[mongoose] ${url} connected successfully`);
+    console.log(`[mongoose] ${url} connected successfully`)
     // logger.info(`[mongoose] ${url} connected successfully`)
   })
 
   db.on('reconnected', () => {
-    console.log(`[mongoose] ${url} reconnected successfully`);
+    console.log(`[mongoose] ${url} reconnected successfully`)
     // logger.info(`[mongoose] ${url} reconnected successfully`)
   })
   return db
